6361db253bf55f14c27ef4f8044308f9973b7b87
Adam Lindberg <ad.lindberg@gmail.com>
2023-12-18 18:06:00 +0100

    first commit

---
+++ API/API.py
@@ -0,0 +1,35 @@
+import requests as rq
+from typing import Union
+
+"""
+This function is used to POST a text to one or all of our models. 
+
+"""
+
+
+def post_to_our_API(text:str, model:Union["svm":str,"sequential":str,"logistic":str,"all_models":str])->dict:
+
+
+    current_models = ["svm",
+                      "sequential",
+                      "logistic",
+                      "all_models"]
+    
+    if model in current_models:
+
+        BASE_URL = f'https://fake-news-version-1-8664e27edd64.herokuapp.com/api/{model}/predict'
+        payload = {"text":text}
+        response = rq.post(BASE_URL, json=payload)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Error:", response.text)
+    else:
+        return f"Model not found"
+
+
+
+
+
+    
